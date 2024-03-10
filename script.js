9c63879d76263e1b353d86ad89336e3b936b82c0
Pranav <pranavhariharan145@gmail.com>
2024-03-10 15:41:37 +0530

    Done with Javascript

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 let input = document.getElementById("textbox")
-let fahrenheit = document.getElementById("")
-
-
+let fahrenheit = document.getElementById("fahrenheit")
+let celcius = document.getElementById("celcius")
+let answer = document.getElementById("ans")
 let val
+
+
 function calculate()
 {
+    if(fahrenheit.checked)
+    {
+        val = Number(input.value)
+        val = 9/5 * val + 32
+        answer.textContent = `Fahrenheit: ${val.toFixed(2)}F`
 
+    }
+    else if(celcius.checked)
+    {
+        val = Number(input.value)
+        val = (val - 32)* 5/9
+        answer.textContent = `Celcius: ${val.toFixed(2)}C`
+
+    }
+    else{
+        answer.textContent = `Select an option from above`
+    }
+
+    
+    
 }
